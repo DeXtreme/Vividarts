6454a548822d3913c9dfb23946ff4d4e8e659ddc
Emmanuel Akolbire <dbakolbire@gmail.com>
2023-12-08 22:33:14 +0000

    Add cors headers and refactor processImage and greyscale

--- src/api/processImage/processImage.py
+++ src/api/processImage/processImage.py
@@ -2,14 +2,38 @@ import json
 import os
 import uuid
 import boto3
+import base64
+from io import BytesIO
 
 def lambda_handler(event, context):
+    headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "OPTIONS,POST",
+    }
+
+    # Check if the request is a preflight request (OPTIONS)
+    if event["httpMethod"] == "OPTIONS":
+        return {
+            "statusCode": 200,
+            "headers": headers,
+            "body": "Preflight request successful",
+        }
+    
     # Extract parameters from the API Gateway request
-    request_type = event.get('type')
-    file_data = event.get('file')
+    body = json.loads(event.get("body"))
+    request_type = body.get('type')
+    file_data = body.get('file')
 
     # Generate a unique image_id
+    result_image_id = str(uuid.uuid4())
     image_id = str(uuid.uuid4())
+
+    # Save the image to S3
+    s3 = boto3.client('s3')
+    s3_bucket = os.environ.get('S3_BUCKET')
+    image = BytesIO(base64.b64decode(file_data))
+    s3.upload_fileobj(image, s3_bucket, f"{image_id}.jpg")
 
     # Check if the type is greyscale
     if request_type.lower() == 'greyscale':
@@ -18,24 +42,25 @@ def lambda_handler(event, context):
         
         if greyscale_function_arn:
             # Invoke the greyscale Lambda function asynchronously
-            invoke_greyscale_lambda(image_id, file_data, greyscale_function_arn)
+            invoke_greyscale_lambda(image_id,result_image_id,greyscale_function_arn)
 
     # Return a JSON response with the image_id
     response = {
-        "image_id": image_id
+        "image_id": result_image_id
     }
 
     return {
         'statusCode': 200,
+        "headers": headers,
         'body': json.dumps(response)
     }
 
-def invoke_greyscale_lambda(image_id, file_data, greyscale_function_arn):
+def invoke_greyscale_lambda(image_id, result_image_id, greyscale_function_arn):
     # Invoke the greyscale Lambda function asynchronously
     client = boto3.client('lambda')
     payload = {
         "image_id": image_id,
-        "file": file_data
+        "result_image_id": result_image_id,
     }
 
     client.invoke(
